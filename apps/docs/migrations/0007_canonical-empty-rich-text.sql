12a3f65249110cfa8fad97397b6e8a9cb2554ad8
jamcaaxian <winsonxianchn@gmail.com>
2026-08-11 00:26:15 +0800

    chore: add SQL formatter and reformat migration files

--- apps/docs/migrations/0007_canonical-empty-rich-text.sql
+++ apps/docs/migrations/0007_canonical-empty-rich-text.sql
@@ -1,6 +1,12 @@
 UPDATE `post`
-SET `body` = json_set(`body`, '$.content[0]', json_object('type', 'paragraph'))
-WHERE json_extract(`body`, '$.type') = 'doc'
-	AND json_array_length(json_extract(`body`, '$.content')) = 1
-	AND json_extract(`body`, '$.content[0].type') = 'paragraph'
-	AND json_array_length(json_extract(`body`, '$.content[0].content')) = 0;
+SET
+	`body` = json_set (
+		`body`,
+		'$.content[0]',
+		json_object ('type', 'paragraph')
+	)
+WHERE
+	json_extract (`body`, '$.type') = 'doc'
+	AND json_array_length (json_extract (`body`, '$.content')) = 1
+	AND json_extract (`body`, '$.content[0].type') = 'paragraph'
+	AND json_array_length (json_extract (`body`, '$.content[0].content')) = 0;
